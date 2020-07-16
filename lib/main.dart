c454dbc967503b33885e8380939579fd3a1a38a9
shinya.takahashi <shinya.takahashi@x-factory.jp>
2020-07-16 13:44:17 +0900

    -

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -32,6 +33,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _bloc = CounterBloc();
+  final _toDoBloc = ToDoBloc();
+
   int _counter = 0;
   List<dynamic> _todoList = [];
 
@@ -66,6 +70,13 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     initializeState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
+    _toDoBloc.dispose();
   }
 
   @override
@@ -126,16 +137,128 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: TextStyle(fontSize: 16.0),
               ),
             ),
+            Container(
+              child: StreamBuilder<int>(
+                  stream: _bloc.counter,
+                  initialData: 100,
+                  builder: (context, snapshot) {
+                    return Text(
+                      '${snapshot.data}',
+                      style: Theme.of(context).textTheme.display1,
+                    );
+                  }),
+            ),
+            Container(
+              child: StreamBuilder<List>(
+                  stream: _toDoBloc.todo,
+                  initialData: [
+                    {
+                      'title': 'default',
+                      'done': false,
+                    }
+                  ],
+                  builder: (context, snapshot) {
+                    List<Widget> todoListWidget = [];
+                    print('snapshot');
+                    print(snapshot.data.runtimeType);
+                    print(snapshot.data[0]);
+                    print(snapshot.data[0]['title']);
+//                    return Container();
+                    for (var i = 0; i < snapshot.data.length; i++) {
+                      final String title = snapshot.data[i]['title'];
+                      final bool done = snapshot.data[i]['done'];
+                      todoListWidget.add(
+                        new CheckboxListTile(
+                          activeColor: Colors.blue,
+                          title: Text(title),
+                          secondary: new Icon(
+                            Icons.thumb_up,
+                            color: done ? Colors.orange[700] : Colors.grey[500],
+                          ),
+                          controlAffinity: ListTileControlAffinity.leading,
+                          onChanged: (bool val) {
+                            print(_todoList);
+                            setState(() {
+                              _todoList[i]['done'] = val;
+                              _countDoneTask();
+                            });
+                          },
+                          value: done,
+                        ),
+                      );
+                    }
+
+                    return Container(
+                      child: Column(
+                        children: todoListWidget,
+                      ),
+                    );
+                  }),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
           _countDoneTask(),
+          _bloc.increment.add(null),
+          _toDoBloc.addTodo.add(null),
         },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class CounterBloc {
+  final _incrementController = StreamController<void>();
+  final _countController = StreamController<int>();
+
+  var _count = 0;
+  CounterBloc() {
+    _incrementController.stream.map((v) => _count++).pipe(_countController);
+  }
+
+  Sink<void> get increment => _incrementController.sink;
+  Stream<void> get counter => _countController.stream;
+
+  void dispose() async {
+    await _incrementController.close();
+    await _countController.close();
+  }
+}
+
+class ToDoBloc {
+  final _addToDoController = StreamController<void>();
+  final _getToDoController = StreamController<List<Map>>();
+
+  var _todoList = [];
+  var _todoList_b = [];
+
+  void init() async {
+    final QuerySnapshot result =
+        await Firestore.instance.collection('todos').getDocuments();
+    final List<DocumentSnapshot> documents = result.documents;
+    documents.forEach((document) {
+      _todoList.add({
+        'title': document.data['title'] as String,
+        'done': document.data['done'] as bool,
+      });
+    });
+  }
+
+  ToDoBloc() {
+    _addToDoController.stream
+        .map((v) => _todoList_b.add(_todoList))
+        .pipe(_getToDoController);
+  }
+
+  Sink<void> get addTodo => _addToDoController.sink;
+  Stream<void> get todo => _getToDoController.stream;
+
+  void dispose() async {
+    await _addToDoController.close();
+    await _getToDoController.close();
   }
 }
